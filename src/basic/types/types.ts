0e7a582f63568b4932f4a3ac364a46283ae55655
JaeHyunGround <gothddlek@naver.com>
2025-12-04 18:06:41 +0900

    refactor : 타입 관리를 위한 types 폴더 생성
    - 한 파일에 함께 작성한 이유 Product 타입이 Discount[] 타입을 사용하는 등 도메인을 넘나들며 타입을 사용하고 있기 때문에 도메인 별로 타입 파일을 따로 생성 후 import 하는 것이 복잡할 것이라고 생각하여 한 파일에서 타입을 관리하고자 헸습니다.

---
+++ src/basic/types/types.ts
@@ -0,0 +1,47 @@
+// product
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  discounts: Discount[];
+}
+
+export interface ProductWithUI extends Product {
+  description?: string;
+  isRecommended?: boolean;
+}
+
+export interface ProductForm {
+  name: string;
+  price: number;
+  stock: number;
+  description: string;
+  discounts: {
+    quantity: number;
+    rate: number;
+  }[];
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+export interface Coupon {
+  name: string;
+  code: string;
+  discountType: "amount" | "percentage";
+  discountValue: number;
+}
+
+export interface Discount {
+  quantity: number;
+  rate: number;
+}
+
+export interface Notification {
+  id: string;
+  message: string;
+  type: "error" | "success" | "warning";
+}
